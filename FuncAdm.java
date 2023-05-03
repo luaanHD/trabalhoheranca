930db28caf2453dfd9874dcbf0fbe1b3b463df8d
luaanHD <luantoniolo94@gmail.com>
2023-05-03 17:37:25 +0000

    Trabalho com Herança com Isaque Katahira

--- FuncAdm.java
+++ FuncAdm.java
@@ -1,4 +1,28 @@
-public class FuncAdm {
+public class FuncAdm extends Funcionario {
+    private String setor;
+    private String funcao;
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+    public String getFuncao() {
+        return funcao;
+    }
     
+    public FuncAdm(String nome, int idade, String endereco, double salario, String setor, String funcao){
+        super(nome, idade, endereco, salario);
+        setSetor(setor);
+        setFuncao(funcao);
+    }
+    public FuncAdm(){
+
+    }
     
 }
---
+++ Principal.java
@@ -0,0 +1,6 @@
+public class Principal {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("José Cláudio Fernandéz", 27, "Rua Changsheng,Liyue - 276", 1250.85);
+        Aluno aluno = new Aluno("Higor Rafael Nando", 17, "Rua Manoel Gomez, Sumeru - 278", "5º Semestre", "Informática para Internet");   
+    }
+}
